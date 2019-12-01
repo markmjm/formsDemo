f54f63999af134799ce6a1852f30a8e1881f88ff
markmjm <markmnei@gmail.com>
2019-12-01 11:31:31 -0500

    dev updates

--- formsApp/forms.py
+++ formsApp/forms.py
@@ -1,5 +1,42 @@
 from django import forms
+
+
 class UserRegistrationForm(forms.Form):
-    firstName: forms.CharField()
-    lastName: forms.CharField()
-    email: forms.CharField()
+    GENDER = [
+        ('male', 'MALE'),
+        ('female', 'FEMALE')
+
+    ] #list of tuples
+    firstName = forms.CharField(widget=forms.TextInput)
+    lastName = forms.CharField(widget=forms.TextInput)
+    email = forms.CharField(widget=forms.TextInput)
+    gender = forms.CharField(widget=forms.Select(choices=GENDER))
+    password = forms.CharField(widget=forms.PasswordInput)
+
+    # custom error message for each field individually
+    ''' 
+    def clean_firstName(self):
+        inputFirstName = self.cleaned_data['firstName']
+        if len(inputFirstName) > 20:
+            raise forms.ValidationError('the max length of firstname must be <= 20 chars')
+        return inputFirstName
+
+    def clean_email(self):
+        inputEmail = self.cleaned_data['email']
+        if inputEmail.find('@') == -1:
+            raise forms.ValidationError('invalid email address')
+        return inputEmail
+    '''
+#single error message for all fields
+    def clean(self):  # overrifr the clean meathion in the clean calls
+        user_cleaned_data = super().clean()
+        inputFirstName = user_cleaned_data['firstName']
+        inputLastName = user_cleaned_data['lastName']
+        inputEmail = user_cleaned_data['email']
+        if len(inputFirstName) > 20 or len(inputLastName) > 20:
+            raise forms.ValidationError('the max length of firstname or lastname must be <= 20 chars')
+        if inputEmail.find('@') == -1:
+            raise forms.ValidationError('invalid email address')
+        return user_cleaned_data
+
+
--- formsApp/views.py
+++ formsApp/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from . import forms
+
 
 # Create your views here.
+def userRegistrationView(request):
+    form = forms.UserRegistrationForm()
+    if request.method == 'POST':
+        form = forms.UserRegistrationForm(request.POST)
+        if form.is_valid():
+            print('Form is valid')
+            print(f"First Name: {form.cleaned_data['firstName']}")
+            print(f"Last Name: {form.cleaned_data['lastName']}")
+            print(f"Email: {form.cleaned_data['email']}")
+    return render(request, 'formsDemo/userRegistration.html', {'form': form})
